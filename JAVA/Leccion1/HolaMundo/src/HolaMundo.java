1f6025081c4de597590c75394669e5c49640605a
Natalia Gutierrez <naty.rcia33@gmail.com>
2022-10-22 11:14:33 -0300

    Agregamos los cambios de Java Leccion1

---
+++ JAVA/Leccion1/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,61 @@
+
+import java.util.Scanner;
+
+
+public class HolaMundo {
+    public static void main(String[] args) {
+       /* System.out.println("Hola Mundo desde Java");
+        
+        int miVariable = 10;
+        System.out.println(miVariable);
+        
+        miVariable = 5;
+        System.out.println(miVariable);
+        //Tipo String
+        String miVariableCadena = "Bienvenidos";
+        System.out.println(miVariableCadena);
+        miVariableCadena = "Sigamos creciendo en Programación";
+        System.out.println(miVariableCadena);
+        */
+       /*
+       //Var - Inferencia de tipos en java
+       var miVariableEntera2 = 10;
+       var miVariableCadena2 = "Seguimos estudiando";
+        System.out.println("miVariableEntera2= " + miVariableEntera2);
+        System.out.println("miVariableCadena2= " + miVariableCadena2);
+        //sout+tab
+        //para ejecutar shift + f6 es la tecla para mayuscula
+        //reglas para definir una variable en java
+        
+        var usuario = "Osvaldo";
+        var titulo = "Ingeniero";
+        var union = titulo + " " + usuario;
+        System.out.println("union = " + union);
+        
+        var a = 8;
+        var b = 4;
+        System.out.println(usuario + (a + b));
+        
+      //Ejercicio Caracteres especiales en Java
+      
+      var nombre = "Natalia";
+        System.out.println("\nNueva Linea: \n" + nombre);//Diagonal Inversa y letra n
+        System.out.println("Tabulador: \t" + nombre);//tabulador un espacio para centrar
+        System.out.println("Retroceso: \b\b" + nombre);//caracter para retroceso en caso que se encuentre muy a la derecha el texto
+        System.out.println("Comillas simples: \'"+nombre+"\'");
+        System.out.println("Comillas Dobles: \""+nombre+"\"");
+        */
+        /*
+        //Clase scanner
+        Scanner entrada = new Scanner (System.in);
+        System.out.println("Digite su nombre: ");
+        var usuario2 = entrada.nextLine();
+        System.out.println("usuario2 = " + usuario2);
+        System.out.println("Escriba el Título");
+        var titulo2 = entrada.nextLine();
+        System.out.println("Resultado: "+titulo2+" "+usuario2);*/
+        
+        
+    }
+        
+}
---
+++ JAVA/Leccion2/CicloWhile/src/ciclowhile/EjercicioWhile01.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ciclowhile;
+
+/**
+ *
+ * @author Nat
+ */
+public class EjercicioWhile01 {
+    public static void main(String[] args) {
+        var conteo = 0; //Inferencia de tipos
+        while (conteo <= 7 ){
+            System.out.println("conteo = " + conteo);
+            conteo++;//incrementa de uno en uno la variable
+        }
+    }
+    
+}
